7cdc33e1147aaa041ce2ff2a33369dd937e6073d
MARCOS ANTONIO ESTREMOTE <estremot@gmail.com>
2026-07-27 11:26:38 -0300

    chore: snapshot automatico dos dados [pre-push]

--- database/seeders/ExportedDataSeeder.php
+++ database/seeders/ExportedDataSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * ExportedDataSeeder — gerado automaticamente em 2026-07-27 14:21:34
+ * ExportedDataSeeder — gerado automaticamente em 2026-07-27 14:26:38
  * Comando: php artisan db:export-seeders
  *
  * Para restaurar: php artisan db:seed --class=ExportedDataSeeder
